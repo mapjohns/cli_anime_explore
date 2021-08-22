6637a7b2dbdc1b4727adb042874fc14f3607de29
Matthew <mattj20@gmail.com>
2021-08-21 20:27:10 -0700

    add desc

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in anime_explore.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/anime_explore.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "anime_explore/version"
+
+module AnimeExplore
+  class Error < StandardError; end
+  # Your code goes here...
+end
