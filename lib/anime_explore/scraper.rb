9f91b3f62d3d632e308301ef7ca5d3d136a34ec5
Matthew <mattj20@gmail.com>
2021-09-11 15:06:53 -0700

    created class/added popularity rank

--- lib/anime_explore/scraper.rb
+++ lib/anime_explore/scraper.rb
@@ -1,48 +1,60 @@
 require 'httparty'
 require 'pry'
 require "./lib/anime_explore/anime.rb"
-# class AnimeExplore::Scraper
-#     attr_accessor :year
-#     @@all = []
-
-#     def initialize(year)
-#         @year = year
-#         parse
-#     end
-
-#     def parse
-#         response = HTTParty.get("https://kitsu.io/api/edge/anime?filter[#{@year}]=2021")
-#         response
-
-# end
-response = HTTParty.get('https://kitsu.io/api/edge/anime?filter[year]=2021')
-@hello = []
-#binding.pry
-def first_test(url)
-    response = HTTParty.get(url)
-    while response["links"]['next']
-        puts response["links"]['next']
-        response = HTTParty.get(response["links"]['next'])
-        response['data'].each do |anime|
-            #@hello << anime["id"]
-            #@hello << anime["attributes"]["canonicalTitle"]
-            AnimeExplore::Anime.new(anime["attributes"]["canonicalTitle"],anime["attributes"]["description"],2021)
-        end
+class Scraper
+    attr_reader :year
+    @@all = []
+    def initialize(year)
+        @year = year
+        parse
     end
-  #  binding.pry
-    list = ["first","last"]
-    list.each do |keyword|
-        response = HTTParty.get(response["links"][keyword])
-        response['data'].each do |anime|
-            @hello << anime["id"]
-            @hello << anime["attributes"]["canonicalTitle"]
+
+    def parse
+        response = HTTParty.get("https://kitsu.io/api/edge/anime?filter[year]=#{@year}")
+        while response["links"]['next']
+            puts response["links"]['next']
+            response = HTTParty.get(response["links"]['next'])
+            response['data'].each do |anime|
+                Anime.new(anime["attributes"]["canonicalTitle"],anime["attributes"]["description"],2021, anime["attributes"]["popularityRank"])
+            end
         end
+        list = ["first","last"]
+        list.each do |keyword|
+            response = HTTParty.get(response["links"][keyword])
+            response['data'].each do |anime|
+                Anime.new(anime["attributes"]["canonicalTitle"],anime["attributes"]["description"],2021, anime["attributes"]["popularityRank"])
+            end
+        end
+        puts "Huge success"
     end
 end
-
-binding.pry
+#Scraper.new(2021)
+# binding.pry
 
 #anime[1][0]["attributes"]["canonicalTitle"] Title of Anime
 #anime[1][0]["attributes"]["description"] Anime Description
 #anime[1][0]["attributes"]["popularityRank"] Popularity Rank
 
+
+# response = HTTParty.get('https://kitsu.io/api/edge/anime?filter[year]=2021')
+# @hello = []
+# #binding.pry
+# def first_test(url)
+#     response = HTTParty.get(url)
+#     while response["links"]['next']
+#         puts response["links"]['next']
+#         response = HTTParty.get(response["links"]['next'])
+#         response['data'].each do |anime|
+#             Anime.new(anime["attributes"]["canonicalTitle"],anime["attributes"]["description"],2021)
+#         end
+#     end
+#   #  binding.pry
+#     list = ["first","last"]
+#     list.each do |keyword|
+#         response = HTTParty.get(response["links"][keyword])
+#         response['data'].each do |anime|
+#             @hello << anime["id"]
+#             @hello << anime["attributes"]["canonicalTitle"]
+#         end
+#     end
+# end
