c01adc422ff0130b7c797922058d2dd8391d439f
Matthew <mattj20@gmail.com>
2021-09-11 15:07:34 -0700

    added popularity rank

--- lib/anime_explore/anime.rb
+++ lib/anime_explore/anime.rb
@@ -1,10 +1,11 @@
 class Anime
-attr_reader :name, :desc, :year
+attr_reader :name, :desc, :year, :popularityrank
 @@all = []
-    def initialize(name, desc, year)
+    def initialize(name, desc, year, popularityrank)
         @name = name
         @desc = desc
         @year = year
+        @popularityrank = popularityrank
         save
     end
 
@@ -16,50 +17,3 @@ attr_reader :name, :desc, :year
         @@all
     end
 end
-
-
-
-
-# require 'httparty'
-# require 'pry'
-# # require "./lib/anime_explore.rb"
-# # class AnimeExplore::Scraper
-# #     attr_accessor :year
-# #     @@all = []
-
-# #     def initialize(year)
-# #         @year = year
-# #         parse
-# #     end
-
-# #     def parse
-# #         response = HTTParty.get("https://kitsu.io/api/edge/anime?filter[#{@year}]=2021")
-# #         response
-
-# # end
-# response = HTTParty.get('https://kitsu.io/api/edge/anime?filter[year]=2021')
-# @hello = []
-# #binding.pry
-# def first_test(url)
-#     response = HTTParty.get(url)
-#     while response["links"]['next']
-#         puts response["links"]['next']
-#         response = HTTParty.get(response["links"]['next'])
-#         response['data'].each do |anime|
-#             #@hello << anime["id"]
-#             #@hello << anime["attributes"]["canonicalTitle"]
-#             Anime.new(anime["attributes"]["canonicalTitle"],anime["attributes"]["description"],2021)
-#         end
-#     end
-#   #  binding.pry
-#     list = ["first","last"]
-#     list.each do |keyword|
-#         response = HTTParty.get(response["links"][keyword])
-#         response['data'].each do |anime|
-#             @hello << anime["id"]
-#             @hello << anime["attributes"]["canonicalTitle"]
-#         end
-#     end
-# end
-
-# binding.pry
