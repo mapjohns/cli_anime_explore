2f87acda641e199e7aa165eb1de2f04da7bd313a
Matthew <mattj20@gmail.com>
2021-09-11 12:56:09 -0700

    created

--- lib/anime_explore.rb
+++ lib/anime_explore.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "anime_explore/version"
+require_relative "anime_explore/cli"
+require_relative "anime_explore/scraper"
+require_relative "anime_explore/anime"
+
+require 'pry'
+require 'httparty'
 
 module AnimeExplore
   class Error < StandardError; end
