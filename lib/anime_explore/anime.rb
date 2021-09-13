24fb35ba09e72d1dc62bdf03b5a57b495ad16895
Matthew <mattj20@gmail.com>
2021-09-12 18:56:39 -0700

    added destroy method

--- lib/anime_explore/anime.rb
+++ lib/anime_explore/anime.rb
@@ -1,3 +1,6 @@
+# require "./lib/anime_explore/scraper.rb"
+# require "./lib/anime_explore/cli.rb"
+require 'pry'
 class Anime
 attr_reader :name, :desc, :year, :popularityrank
 @@all = []
@@ -8,12 +11,15 @@ attr_reader :name, :desc, :year, :popularityrank
         @popularityrank = popularityrank
         save
     end
-
     def save
         @@all << self
     end
 
     def self.all
         @@all
+    end
+
+    def self.destroy
+        @@all.clear()
     end
 end
