f2bcf0ea374bb4d0c2cdbca2de2366a61d70cf6d
Matthew <mattj20@gmail.com>
2021-09-19 19:41:05 -0700

    Added status and dates

--- lib/anime_explore/anime.rb
+++ lib/anime_explore/anime.rb
@@ -1,14 +1,15 @@
-# require "./lib/anime_explore/scraper.rb"
-# require "./lib/anime_explore/cli.rb"
-require 'pry'
-class Anime
-attr_reader :name, :desc, :year, :popularityrank
+class AnimeExplore::Anime
+attr_reader :name, :desc, :year, :popularityrank, :episodecount, :status, :startDate, :endDate
 @@all = []
-    def initialize(name, desc, year, popularityrank)
+    def initialize(name, desc, year, popularityrank, episodecount,status, startDate, endDate)
         @name = name
         @desc = desc
         @year = year
         @popularityrank = popularityrank
+        @episodecount = episodecount
+        @status = status
+        @startDate = startDate
+        @endDate = endDate
         save
     end
     def save
